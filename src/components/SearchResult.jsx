591fd6f5b462818f4ddb3942ef56a941b23b7f81
Ornerd <sharritsdesigns@gmail.com>
2024-06-21 01:14:10 +0100

    fixes on suggestion tray, genre addition to crads, and more CSS

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const SearchResult = ({poster_path, title, release_date, overview, id}) => {
+const SearchResult = ({poster_path, title, release_date, overview, id, genre_ids , genres}) => {
   const API_IMG = process.env.REACT_APP_API_IMG;
 
   return (
-    <main>
+    <section className='search-result'>
         <div>
-        <img data-testid="movie-poster" src={API_IMG+poster_path !=="https://image.tmdb.org/t/p/w500/null"? API_IMG+poster_path: 'https://via.placeholder.com/400x600'} alt={title}/>
+          <Link to={`/${id}`}>
+            <img data-testid="movie-poster" src={API_IMG+poster_path !=="https://image.tmdb.org/t/p/w500/null"? API_IMG+poster_path: 'https://via.placeholder.com/400x600'} alt={title}/>
+          </Link>
         </div>
         <div>
-            <Link to={`/${id}`}><h2>{title}</h2></Link>
-            <h4>date released:{release_date} </h4>
-            <h6></h6>
-            <p>{overview}</p>
+          <Link to={`/${id}`}><h2>{title}</h2></Link>
+          <h4>date released: <i>{release_date}</i> </h4>
+          <span>
+            {genre_ids.map((id) => {
+              const genre = genres.find(g => g.id === id);
+              return genre ? <h6 key={genre.id}>{genre.name}</h6> : null; 
+            })}
+          </span>
+          
+          <p>{overview}</p>
         </div>
-    </main>
+    </section>
   )
 }
 
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -4,8 +4,10 @@ import SearchIcon from '../asset/icons/search_icon copy.svg';
 import Logo from '../asset/icons/Logo black.png'
 import HamMenu from '../asset/icons/Menu.svg';
 import '../asset/css/searchResults.css';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams, useNavigation } from 'react-router-dom';
 import SearchResult from '../components/SearchResult'
+import SuggestedWord from '../components/SuggestedWord';
+import useDebounce from '../hooks/useDebounce';
 
 const SearchResults = () => {
     
@@ -14,10 +16,17 @@ const SearchResults = () => {
     const searchKeyword = searchParam.get("search")
 
     const API_URL_for_search = process.env.REACT_APP_API_URL_for_search
+    const API_URLtwo = process.env.REACT_APP_API_URLtwo
 
     const [movieKeyword, setMovieKeyword] = useState(searchKeyword)
     const [displayedKeyword, setDisplayedKeyword] = useState(searchKeyword) //for the 'search results for' area
     const [movieResults, setMovieResults] = useState([])
+    const [displayedAmount, setDisplayedAmount] = useState(6)
+    const [moviesSuggestionList, setMoviesSuggestionList] = useState([]);
+    const [genres, setGenres] = useState([])
+    const debouncedSearchterm = useDebounce(movieKeyword, 500);
+    const uniqueTitlesSet = new Set();
+    const navigationHook = useNavigation()
 
 
     const fetchSearchedMovie = async (query) => {
@@ -35,13 +44,20 @@ const SearchResults = () => {
         if (searchKeyword) {
             fetchSearchedMovie(movieKeyword)
             .then(results => {
-                setMovieResults(results.slice(0,12))
-                // setMoviesSuggestionList([])
+                setMovieResults(results)
             })
             .catch(error => {
                 console.error('Error fetching more movies:', error);
             });
         }
+
+        fetch(API_URLtwo)
+        .then((res)=>res.json())
+        .then(data=>{
+            console.log(data.genres)
+            setGenres(data.genres)
+        })
+
     }, [])
 
     async function SearchForMovies(){
@@ -49,12 +65,12 @@ const SearchResults = () => {
         .then(results => {
             setMovieResults(results.slice(0,12)) 
             setDisplayedKeyword(movieKeyword) 
-            // setMoviesSuggestionList([])
+            setMoviesSuggestionList([])
         })
         .catch(error => {
             console.error('Error fetching more movies:', error);
         });
-
+        setDisplayedAmount(6)
     }
 
     const handleKeyDown = (e) => {
@@ -62,19 +78,82 @@ const SearchResults = () => {
             e.preventDefault();
             fetchSearchedMovie(movieKeyword)
             .then(results => {
-                setMovieResults(results.slice(0,12))
+                setMovieResults(results)
                 setDisplayedKeyword(movieKeyword) 
-                // setMoviesSuggestionList([])
+                setMoviesSuggestionList([])
             })
             .catch(error => {
                 console.error('Error fetching more movies:', error);
             });
         }
         setSearchParam({search: `${movieKeyword}`})
+        setDisplayedAmount(6)
     };
 
+    useEffect(()=> {
+        async function loadMovieSuggestions(){
+
+            fetchSearchedMovie(debouncedSearchterm)
+            .then(results => {
+                if(debouncedSearchterm !== searchKeyword) {
+                    setMoviesSuggestionList(results)
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching more movies:', error);
+            });
+              
+        }
+
+        if (debouncedSearchterm) {
+            loadMovieSuggestions();
+        }  
+    }, [debouncedSearchterm])
+
+    useEffect(()=> {
+        if(movieKeyword === '') {
+            setMoviesSuggestionList([])
+        }
+    }, [movieKeyword])
+
+    function showMoreResults() {
+        setDisplayedAmount((prev)=> prev + 6)
+    }
+
+    // Use filter to keep only unique movie titles while maintaining the order
+    const uniqueMovies = moviesSuggestionList.filter(movie => {
+        // Convert movie to lowercase for case-insensitive comparison
+        const lowerCaseMovie = movie.title.toLowerCase();
+
+        // Check if the lowercase movie title is already in the Set
+        if (!uniqueTitlesSet.has(lowerCaseMovie)) {
+            uniqueTitlesSet.add(lowerCaseMovie); // If not, add it to the Set
+            return true; // Keep the movie title in the filtered array
+        } else {
+            return false; // Skip the duplicate movie title
+        }
+    }).slice(0,7);
+
+    const handleSearch = async (suggestion) => {  //a little something I see on google. When a suggested word is clicked, the search bar acts on that word to produce results.          
+        
+        // navigateTo(`/search?search=${suggestion.title}`)
+        fetchSearchedMovie(suggestion.title)
+        .then(results => {
+            setMovieResults(results.slice(0,12)) 
+            setMoviesSuggestionList([])
+        })
+        .catch(error => {
+            console.error('Error fetching more movies:', error);
+        });
+
+    } 
+
+
   return (
-    <main className='results-area'>
+    <main className={navigationHook.state === "loading"? "results-area no-interactions": "results-area"} onClick={()=> setMoviesSuggestionList([])}>
+
+    <div className={navigationHook.state === "loading"? "loading": ""}></div>
+
       <nav>
         <Link to='/' style={{display:'flex', alignItems: 'center'}}>
         <img className="logo"
@@ -98,9 +177,9 @@ const SearchResults = () => {
                 onClick= {()=> {SearchForMovies(); setSearchParam({search: `${movieKeyword}`})}}                        
             />
         </form>
-        {/* <div className='sugggested-word-container'>
-            {SearchMovies? uniqueMovies.map((suggestions)=> <SuggestedWord key={suggestions.id} suggestions={suggestions} handleClick={handleSearch}/>): <div></div>}
-        </div> */}
+        <div className='sugggested-word-container'>
+            {movieKeyword? uniqueMovies.map((suggestion)=> <SuggestedWord key={suggestion.id} suggestion={suggestion} handleClick={handleSearch}/>): <div></div>}
+        </div>
         </div>
         
                       
@@ -116,15 +195,17 @@ const SearchResults = () => {
 
     <h1>Search results for <i>{displayedKeyword}</i></h1>
 
-    <section>
+    <section className='results-container'>
         {
             movieResults?.length > 0?
-            movieResults.map((movieResult)=> {
-                return <SearchResult key={movieResult.id}{...movieResult}/>
+            movieResults.slice(0, displayedAmount).map((movieResult)=> {
+                return <SearchResult genres={genres} key={movieResult.id}{...movieResult}/>
             })
             :
             <h2>No results found.</h2> 
         }
+
+        <h4 className={displayedAmount > movieResults.length? "see-more no-interactions" :"see-more"} onClick={()=>{showMoreResults()}}>See more</h4>
     </section>
 
     <Footer/>
